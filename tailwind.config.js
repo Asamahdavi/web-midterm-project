9963ba26bdc5aeb11a78e2fe17d1663cc95fae84
Asa.mahdavi <Asamahdavi22@gmial.com>
2021-12-18 21:18:48 +0330

    gh pages build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-module.exports = {  content: ["./src/**/*.{html,js}"],  theme: {    extend: {},  },  plugins: [],}
+
+module.exports = {  content: ["./src/**/*.{html,js}"],  theme: {
+    extend: {
+      spacing: {
+        '1/5': '20%',
+      },
+    },
+  },
+  variants: {},
+  plugins: [], 
+
+}
